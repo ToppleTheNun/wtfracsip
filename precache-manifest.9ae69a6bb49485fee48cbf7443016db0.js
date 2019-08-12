63d2944a638bbb3f3fe87faf30c68d78d7018ac2
Richard Harrah <topplethenunnery@gmail.com>
2019-08-12 10:50:26 -0400

    Updates

---
+++ precache-manifest.9ae69a6bb49485fee48cbf7443016db0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eacb8328df867a60a88a4ea02cc1175",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9c46c2ad25938d89332",
+    "url": "/static/css/main.9820653d.chunk.css"
+  },
+  {
+    "revision": "7f64db4ee07366139bd1",
+    "url": "/static/js/2.8e5e124a.chunk.js"
+  },
+  {
+    "revision": "e9c46c2ad25938d89332",
+    "url": "/static/js/main.c7c7aa5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
